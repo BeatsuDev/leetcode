76e22623f4bb95efb11ded2316a9509b6ece705b
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-10-13 23:38:46 +0200

    task(167): add solution for two sum II

---
+++ 167. Two Sum II/main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+# Time complexity: O(n log(n))
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        last_index = len(numbers)-1
+        for i, n in enumerate(numbers):
+            compliment_index = self.binary_search(numbers, i+1, last_index, target-n)
+            if compliment_index > 0:
+                return (i+1, compliment_index+1)
+        
+    def binary_search(self, numbers: List[int], start: int, end: int, target: int) -> int:
+        if start == end:
+            if numbers[start] == target:
+                return start
+            return -1
+        
+        middle = (end + start) // 2
+        if target > numbers[middle]:
+            return self.binary_search(numbers, middle+1, end, target)
+        return self.binary_search(numbers, start, middle, target)
