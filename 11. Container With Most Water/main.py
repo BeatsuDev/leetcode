350b9ee30a7b2d2e72a6cdc870b2fc4062a7ef6c
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-10-15 05:07:54 +0200

    task(11): add solution for container with most water

---
+++ 11. Container With Most Water/main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height)-1
+
+        largest_number = max(height)
+        largest = 0
+
+        # While it is still possible to find a greater sum
+        while (right-left)*largest_number > largest:
+            area = min(height[left], height[right]) * (right - left)
+            if area > largest:
+                largest = area
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return largest
